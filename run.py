653aa10653de5a169523aea5a50564bd0e26428e
root <yzhangqg@connect.ust.hk>
2025-08-20 01:25:31 +0000

    evaluation

---
+++ run.py
@@ -0,0 +1,20 @@
+import torch
+from vggt.models.vggt import VGGT
+from vggt.utils.load_fn import load_and_preprocess_images
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# bfloat16 is supported on Ampere GPUs (Compute Capability 8.0+) 
+dtype = torch.bfloat16 if torch.cuda.get_device_capability()[0] >= 8 else torch.float16
+
+# Initialize the model and load the pretrained weights.
+# This will automatically download the model weights the first time it's run, which may take a while.
+model = VGGT.from_pretrained("facebook/VGGT-1B").to(device)
+
+# Load and preprocess example images (replace with your own image paths)
+image_names = ["examples/kitchen/images/00.png", "examples/kitchen/images/01.png", "examples/kitchen/images/02.png"]  
+images = load_and_preprocess_images(image_names).to(device)
+
+with torch.no_grad():
+    with torch.cuda.amp.autocast(dtype=dtype):
+        # Predict attributes including cameras, depth maps, and point maps.
+        predictions = model(images)
